5debd9c7b21aedf7dd5818365edcfa4de1e106b1
Peter Mei <peter.mei@autoalert.com>
2019-10-11 14:31:36 -0700

    Convert CouresPage component from Component class to function component

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,42 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { getCourses } from "../api/courseApi";
 
-class CoursesPage extends React.Component {
-    state = {
-        courses: []
-    };
-
-    componentDidMount() {
-        getCourses().then(courses => this.setState({courses: courses}));
-    }
+function CoursesPage() {
+    const [courses, setCoures] = useState([]);
     
-    render() {
-        return ( 
-            <>
-                <h2>Courses</h2>
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th>Title</th>
-                            <th>Author ID</th>
-                            <th>Category</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.courses.map(course => {
-                            return (
-                                <tr>
-                                    <td>{course.title}</td> 
-                                    <td>{course.authorId}</td>
-                                    <td>{course.category}</td>
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </table>
-            </>
-        )
-    }
+    useEffect(() => {
+      getCourses().then(_course => setCoures(_course));
+    }, [])
+    
+    return ( 
+        <>
+            <h2>Courses</h2>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Author ID</th>
+                        <th>Category</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {courses.map(course => {
+                        return (
+                            <tr>
+                                <td>{course.title}</td> 
+                                <td>{course.authorId}</td>
+                                <td>{course.category}</td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </>
+    )
+    
 }
 
 export default CoursesPage;
