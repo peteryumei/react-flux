f61a8a8fddc57db15c14cadcdc4a353fec2f2180
Peter Mei <petermei@yahoo.com>
2019-10-10 22:51:15 -0700

    Add courses page and get courses data from Mock API

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 function Header() {
-    return (
-        <nav>
-            <a href="/">Home</a> | <a href="/about">About</a>
-        </nav>
-    )
+  return (
+    <nav>
+      <a href="/">Home</a> | <a href="/courses">Courses</a>{" "}
+      | <a href="/about">About</a> | 
+      
+    </nav>
+  );
 }
 
 export default Header;
