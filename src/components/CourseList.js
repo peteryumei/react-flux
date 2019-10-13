c8465fa4e4df2e3853404e2a613641e15c8120bc
Peter Mei <petermei@yahoo.com>
2019-10-12 17:45:44 -0700

    Add Course Form

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -33,7 +33,7 @@ CourseList.propTypes = {
   courses: PropTypes.arrayOf(PropTypes.shape({
     id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
-    authorId: PropTypes.string.isRequired,
+    authorId: PropTypes.number.isRequired,
     category: PropTypes.string.isRequired
   })).isRequired
 }
--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useState} from "react";
 import { Prompt } from "react-router-dom";
+import CourseForm from "./CourseForm";
+import * as courseApi from "../api/courseApi";
+import { toast } from 'react-toastify';
 
 const ManageCoursePage = props => {
+  const [errors, setErrors] = useState({});
+  const [course, setCourse ] = useState({
+    id: null,
+    slug: "",
+    title: "",
+    authorId: null,
+    category: ""
+  });
+
+  function handleChange({target}) {
+    //debugger
+    const updatedCourse = {...course, [target.name]: target.value};
+    setCourse(updatedCourse);
+  }
+
+
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    courseApi.saveCourse(course).then( () => {
+      props.history.push("/courses");
+      toast.success("Course Saved.")
+    });
+  }
+
   return (
     <>
       <h2>Manage Course</h2>
       <Prompt when={true} message="Are you sure you want to leave?" />
-      {props.match.params.slug}
+      <CourseForm 
+        course={course} 
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+        />
     </>
   );
 };
